72380818610fb84e4c55ad4e7cf3d843a3c1cd3d
Hos <hos33456@gmail.com>
2025-02-06 03:08:21 +0900

    최초

--- apps/test/test.py
+++ apps/test/test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)  # Flask 애플리케이션 객체 생성
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask!!"
 
 if __name__ == "__main__":
     app.run(debug=True)     # flask run을 사용할 경우 해당 설정이 적용 X -> flask --debug run 사용
